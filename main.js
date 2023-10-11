faa42a6026b1004c1a7f532cd2a0b04653ce4894
Vitoria <vazevedo73@gmail.com>
2023-10-11 12:27:36 -0300

    botão pronto

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 'use strict'
 
+
+/**  Menu  **/
 const openMenuBtn = document.getElementById('open-menu-btn')
 const closeMenuBtn = document.getElementById('close-menu-btn')
 const menu = document.getElementById('menu')
@@ -14,7 +16,40 @@ const closeMenu = function(){
 
 openMenuBtn.addEventListener('click', openMenu)
 closeMenuBtn.addEventListener('click', closeMenu)
+/**   **/
 
 
-const buttonRandomVerse = document.getElementById('btn-generate-random-verse')
+/** Botão - Gerar versículo aleatório **/
+const buttonRandomVerse = document.getElementById('btnGenerateRandomVerse')
+
+const getRandomVerse = async function(){
+    const url = 'https://www.abibliadigital.com.br/api/verses/nvi/random'
+    const response = await fetch(url)
+    const returnVerse = await response.json()
+    return returnVerse
+}
+
+const showRandomVerse = async function(){
+    const showVerses = document.getElementById('aparecer-versiculo')
+
+    const btnGenerateRandomVerse = await getRandomVerse(buttonRandomVerse.value)
+
+    showVerses.value = `Livro: ${btnGenerateRandomVerse.book.name}\nCapítulo: ${btnGenerateRandomVerse.chapter}\n Versículo: ${btnGenerateRandomVerse.number}\n Texto: ${btnGenerateRandomVerse.text}`
+}
+
+buttonRandomVerse.addEventListener('click', showRandomVerse)
+/**   **/
+
+
+/** Pesquisa **/
 const inputPesquisar = document.getElementById('pesquisar')
+
+const getBooks = async function(books){
+    const url = `https://www.abibliadigital.com.br/api/${books}/`
+    const response = await fetch(url)
+    const returnBook = await response.json()
+    console.log(returnBook)
+    return returnBook
+}
+
+inputPesquisar.addEventListener('click', getBooks)
