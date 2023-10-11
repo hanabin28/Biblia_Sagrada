c30a4405084fc96d85efac370d1818c980f7d82a
Vitoria Azevedo <giseli.cruz@PAREXGROUP.CORP>
2023-10-11 00:38:52 -0300

    menu quase pronto

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 'use strict'
 
+const openMenuBtn = document.getElementById('open-menu-btn')
+const closeMenuBtn = document.getElementById('close-menu-btn')
+const menu = document.getElementById('menu')
+
+const openMenu = function(){
+    menu.classList.add('active')    
+}
+
+const closeMenu = function(){
+    menu.classList.remove('active')
+}
+
+openMenuBtn.addEventListener('click', openMenu)
+closeMenuBtn.addEventListener('click', closeMenu)
+
+
 const buttonRandomVerse = document.getElementById('btn-generate-random-verse')
 const inputPesquisar = document.getElementById('pesquisar')
