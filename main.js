7894c3275fb774dd1adfc03a7ea45daf59c9eec8
Vitoria Azevedo <giseli.cruz@PAREXGROUP.CORP>
2023-10-18 01:05:39 -0300

    .

--- main.js
+++ main.js
@@ -7,32 +7,43 @@ const closeMenuBtn = document.getElementById('close-menu-btn')
 const menu = document.getElementById('menu')
 const bodyChange = document.getElementById('body')
 
-const openMenu = function(){
+const openMenu = function () {
     menu.classList.add('active')
     bodyChange.classList.add('change')
 }
 
-const closeMenu = function(){
+const closeMenu = function () {
     menu.classList.remove('active')
     bodyChange.classList.remove('change')
 }
 
 openMenuBtn.addEventListener('click', openMenu)
 closeMenuBtn.addEventListener('click', closeMenu)
+
+/** Menu - dropdown **/
+const dropdown = document.getElementById('dropdown')
+const subMenu = document.getElementById('sub-menu')
+
+const openSubMenu = function () {
+    subMenu.classList.add('show')
+}
+
+dropdown.addEventListener('click', openSubMenu)
+
 /**   **/
 
 
 /** Botão - Gerar versículo aleatório **/
 const buttonRandomVerse = document.getElementById('btnGenerateRandomVerse')
 
-const getRandomVerse = async function(){
+const getRandomVerse = async function () {
     const url = 'https://www.abibliadigital.com.br/api/verses/nvi/random'
     const response = await fetch(url)
     const returnVerse = await response.json()
     return returnVerse
 }
 
-const showRandomVerse = async function(){
+const showRandomVerse = async function () {
     const showVerses = document.getElementById('aparecer-versiculo')
 
     const btnGenerateRandomVerse = await getRandomVerse(buttonRandomVerse.value)
@@ -47,22 +58,21 @@ buttonRandomVerse.addEventListener('click', showRandomVerse)
 /** Pesquisa por livro **/
 const inputPesquisar = document.getElementById('pesquisar')
 
-const getBooks = async function(books){
+const getBooks = async function (books) {
     const url = `https://www.abibliadigital.com.br/api/books/${books}`
     const response = await fetch(url)
     const returnBook = await response.json()
     return returnBook
 }
 
-const showBooks = async function(){
+const showBooks = async function () {
     const showBooks = document.getElementById('aparecer-versiculo')
     const books = await getBooks(inputPesquisar.value)
 
     showBooks.value = `Livro: ${books.name}\n Abreviação: ${books.abbrev.pt}\nAutor: ${books.author}\nCapítulos: ${books.chapters}\n${books.testament}\nGrupo: ${books.group}\n${books.comment}`
 }
 
-// inputPesquisar.addEventListener('blur', showBooks)
-inputPesquisar.addEventListener('keypress', (event) =>{
+inputPesquisar.addEventListener('keypress', (event) => {
     if (event.code === 'Enter') {
         showBooks()
     }
@@ -71,21 +81,22 @@ inputPesquisar.addEventListener('keypress', (event) =>{
 
 
 /** Pesquisa - livro + capítulo **/
-const getChapter = async function(chapter){
-    const url = `https://www.abibliadigital.com.br/api/verses/nvi/gn/${chapter}`
+const getChapter = async function (chapter) {
+    const url = `https://www.abibliadigital.com.br/api/verses/nvi/${getBooks(books)}/${chapter}`
     const response = await fetch(url)
     const returnChapter = await response.json()
     console.log(returnChapter)
     return returnChapter
 }
 
-const showChapter = async function(){
+const showChapter = async function () {
     const showChapter = document.getElementById('aparecer-versiculo')
     const chapter = await getChapter(inputPesquisar.value)
+    const books = await showBooks()
 
     showChapter.value = chapter.book.name
-    showChapter.value = chapter.verses[5].text
-    
+    // showChapter.value = chapter.verses[5].text
+
 }
 
 inputPesquisar.addEventListener('blur', showChapter)
